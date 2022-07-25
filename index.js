8f563211c6f29d6c9cedd41acbd857ecbee2c9ee
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-07-26 03:17:05 +0600

    services post & get api

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ require('dotenv').config()
 
 const port = 5500 || process.env.PORT
 
+// cors && middleware
+app.use(express.json())
+const cors = require('cors')
+app.use(cors())
+
+
 // mongo client
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zo2yn.mongodb.net/?retryWrites=true&w=majority`;
@@ -11,7 +17,26 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 // connect mongodb 
 async function connect() {
-    await client.connect() ? console.log('connected') : console.log('not connected')
+    await client.connect() ? console.log('connected') : console.log('not connected');
+
+    // collections
+    const servicesCollection = client.db('manufacturer').collection('services');
+
+    //  post api 
+    app.post('/api/services', async (req, res) => {
+        const service = req.body;
+        await servicesCollection.insertOne(service);
+        res.send(service);
+    });
+
+    // get api
+    app.get('/api/services', async (req, res) => {
+        const services = await servicesCollection.find({}).sort({ $natural: -1 }).toArray();
+        res.send(services);
+    })
+
+
+
 }
 connect().catch(console.dir);
 
