e048c9c0e44f035cc37f16e34d402ce09b71fe55
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-07-28 13:46:28 +0600

    admin set api

--- index.js
+++ index.js
@@ -131,6 +131,17 @@ async function connect() {
         res.send(users);
     });
 
+    // admin put api
+  app.put('/user/admin/:email', verifyToken,  async (req, res) => {
+    const email = req.params.email;
+    const requester = req.decoded.email;
+    const requesterAccount = await usersCollection.findOne({ email: requester });
+    const filter = { email: email };
+    const updateDoc = { $set: { role: 'admin' } };
+    const result = await usersCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  })
+
 
     // newsletter post api
     app.post('/api/newsletter/:email', async (req, res) => {
